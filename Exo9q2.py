fcb91f41d6b007f5face489c54494c910d0af198
Cheikh Mbow <cheikh2mbow@gmail.com>
2020-05-21 15:55:03 +0000

    second commit

--- Exo9q2.py
+++ Exo9q2.py
@@ -3,12 +3,20 @@ md= int(input("Entrer minutes de depart = "))
 ha= int(input("Entrer l'heure d'arrivée = "))
 ma= int(input("Entrer minutes d'arrivée = "))
 
-if hd > 0 or hd <  24 or md > 0 or md < 60 and ha > 0 or ha <  24 or ma > 0 or ma < 60:
+if ha > hd:
+    if ma > md:
+        dha = ha-hd
+        dma = ma - md
+    else:
+        dha = ha-hd-1
+        dma = ma + 60 - md
+else:
     if ma > md:
         dha = ha-hd+24
         dma = ma - md
     else:
         dha = ha-hd+24-1
         dma = ma + 60 - md
-    print("la durée du vol est : "+str(dha)+" heures et "+str(dma)+" minutes ")
+        
+print("la durée du vol est : "+str(dha)+" heures et "+str(dma)+" minutes ")
 
