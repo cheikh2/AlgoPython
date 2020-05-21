2f7b5bbad6c1bb1ac2a13745b40498eba5f56cbd
Cheikh Mbow <cheikh2mbow@gmail.com>
2020-05-21 15:17:25 +0000

    second commit

--- Exo9q2.py
+++ Exo9q2.py
@@ -1,16 +1,14 @@
-def main():
-    hd= int(input("Entrer l'heure de depart = "))
-    md= int(input("Entrer minutes de depart = "))
-    ha= int(input("Entrer l'heure d'arrivée = "))
-    ma= int(input("Entrer minutes d'arrivée = "))
+hd= int(input("Entrer l'heure de depart = "))
+md= int(input("Entrer minutes de depart = "))
+ha= int(input("Entrer l'heure d'arrivée = "))
+ma= int(input("Entrer minutes d'arrivée = "))
 
-    while hd > 0 or hd <  24 or md > 0 or md < 60 and ha > 0 or ha <  24 or ma > 0 or ma < 60:
-        if ha <= hd:
-            dha = 24 - hd + ha
-	        dma = md + ma 
-        
+if hd > 0 or hd <  24 or md > 0 or md < 60 and ha > 0 or ha <  24 or ma > 0 or ma < 60:
+    if ma > md:
+        dha = ha-hd+24
+        dma = ma - md
+    else:
+        dha = ha-hd+24-1
+        dma = ma + 60 - md
     print("la durée du vol est : "+str(dha)+" heures et "+str(dma)+" minutes ")
-    
-if __name__ == "__main__":
-    main()
 
